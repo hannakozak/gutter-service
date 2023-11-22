2f1d80b160b85ffa074491c5924187b3cc391448
hanna <hannawkozak@gmail.com>
2023-11-22 11:28:53 +0000

    refactor: refactor feature component

---
+++ src/data/features.ts
@@ -0,0 +1,25 @@
+import { Hammer, Shovel, Trash2 } from "lucide-react";
+
+export const features = [
+	{
+		id: 1,
+		heading: "Debris Removal",
+		description:
+			"The primary goal of gutter cleaning is to remove debris such as leaves, twigs, dirt, and other buildup from the gutters. This prevents clogs and allows water to flow freely.",
+		icon: Trash2,
+	},
+	{
+		id: 2,
+		heading: "Downspout Cleaning",
+		description:
+			"Cleaning the downspouts is crucial for ensuring that water is properly directed away from the home foundation. Professionals will clear any obstructions in the downspouts.",
+		icon: Shovel,
+	},
+	{
+		id: 3,
+		heading: "Gutter Inspection",
+		description:
+			"In addition to cleaning, a thorough inspection of the gutters is often performed. This helps identify any damage, rust, or sagging that may require repairs.",
+		icon: Hammer,
+	},
+];
