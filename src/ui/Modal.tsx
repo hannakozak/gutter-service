64d17817bf09359505d4e721bc8bc7a162bf13ed
hanna <hannawkozak@gmail.com>
2023-12-28 23:08:43 +0000

    fix: responsivness

--- src/ui/Modal.tsx
+++ src/ui/Modal.tsx
@@ -3,7 +3,7 @@ import { Contact } from "./contact/contact";
 export const Modal = () => {
 	return (
 		<div className="fixed inset-0 z-10 flex h-full w-full items-center justify-center overflow-y-auto bg-gray-800 bg-opacity-50">
-			<div className="rounded-md border bg-white p-8 shadow-lg sm:w-[400px]">
+			<div className="w-[280px] rounded-md border bg-white p-8 shadow-lg sm:w-[400px]">
 				<Contact />
 			</div>
 		</div>
